e7fa19b265dfc23165a065960e0f01a44c967008
laiyuling <laiyuling424@163.com>
2019-07-09 19:14:49 +0800

    7.9 third

--- wanandroid/src/main/java/com/lyl/wanandroid/ui/fragment/first/main/MainFragment.kt
+++ wanandroid/src/main/java/com/lyl/wanandroid/ui/fragment/first/main/MainFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.viewpager.widget.PagerAdapter
+import com.bumptech.glide.Glide
 import com.lyl.wanandroid.R
 import com.lyl.wanandroid.ui.base.BaseFragment
-import kotlinx.android.synthetic.main.main_fragment_layout.*
+import com.lyl.wanandroid.WanAdnroidApplication.Companion.context
+
 
 /**
  * User: lyl
@@ -14,8 +18,10 @@ import kotlinx.android.synthetic.main.main_fragment_layout.*
  */
 class MainFragment:BaseFragment(){
 
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.main_fragment_layout, container, false)
+        val view = inflater.inflate(com.lyl.wanandroid.R.layout.main_fragment_layout, container, false)
         return view
     }
 
@@ -23,5 +29,26 @@ class MainFragment:BaseFragment(){
         super.onActivityCreated(savedInstanceState)
     }
 
+    internal class Adapter : PagerAdapter() {
+        override fun isViewFromObject(view: View, `object`: Any): Boolean {
+            return view==`object`
+        }
+
+        override fun getCount(): Int {
+           return Int.MAX_VALUE
+        }
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            var view= LayoutInflater.from(context).inflate(R.layout.banner_viewpager,null);
+            var img= view.findViewById<ImageView>(R.id.img_banner)
+            Glide.with(context).load("url").into(img)
+            container.addView(view)
+            return view
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            container.removeView(`object` as View)
+        }
+    }
 }
 
