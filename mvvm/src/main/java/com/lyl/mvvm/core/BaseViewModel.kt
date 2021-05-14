57fd251044aacf68ba47c4259ba187a02f5458c5
laiyuling <laiyuling424@163.com>
2021-05-14 14:01:36 +0800

    add mvvm second no test

--- mvvm/src/main/java/com/lyl/mvvm/core/BaseViewModel.kt
+++ mvvm/src/main/java/com/lyl/mvvm/core/BaseViewModel.kt
@@ -11,36 +11,14 @@ import java.lang.Exception
  */
 open class BaseViewModel : ViewModel(){
 
-    private val viewModelJob = SupervisorJob()
-    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
-
-    override fun onCleared() {
-        super.onCleared()
-        viewModelJob.cancel()
-    }
-
-
     private val error by lazy { MutableLiveData<Exception>() }
 
     private val finally by lazy { MutableLiveData<Int>() }
 
     //运行在UI线程的协程
-//    fun launchUI(block: suspend CoroutineScope.() -> Unit) = viewModelScope.launch {
-//        try {
-//            withTimeout(5000){
-//                block()
-//            }
-//        } catch (e: Exception) {
-//            error.value = e
-//        } finally {
-//            finally.value = 200
-//        }
-//    }
-
-    //运行在UI线程的协程
-    fun launchUI( block: suspend CoroutineScope.() -> Unit) {
+    fun launchUI(block: suspend CoroutineScope.() -> Unit) = viewModelScope.launch {
         try {
-            uiScope.launch(Dispatchers.Main) {
+            withTimeout(5000){
                 block()
             }
         } catch (e: Exception) {
@@ -49,6 +27,7 @@ open class BaseViewModel : ViewModel(){
             finally.value = 200
         }
     }
+
 
     /**
      * 请求失败，出现异常
--- mvvm/src/main/java/com/lyl/mvvm/test/ScrollingViewModel.kt
+++ mvvm/src/main/java/com/lyl/mvvm/test/ScrollingViewModel.kt
@@ -2,7 +2,9 @@ package com.lyl.mvvm.test
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.lyl.mvvm.core.BaseViewModel
+import kotlinx.coroutines.*
 
 class ScrollingViewModel : BaseViewModel() {
 
@@ -20,4 +22,39 @@ class ScrollingViewModel : BaseViewModel() {
             val result = repository.getDatas()
             datas.value = result.data
     }
+
+
+//    fun a(){
+//        viewModelScope.launch {
+//            withContext(Dispatchers.Main){
+//
+//            }
+//        }
+//
+//        GlobalScope.launch {
+//            withContext(Dispatchers.Main){
+//
+//            }
+//        }
+//
+//        MainScope().launch {
+//            withContext(Dispatchers.Main){
+//
+//            }
+//        }
+//    }
+//
+//
+//    private val viewModelJob = SupervisorJob()
+//    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+//    //运行在UI线程的协程
+//    fun launchUIX( block: suspend CoroutineScope.() -> Unit) {
+//        try {
+//            uiScope.launch(Dispatchers.Main) {
+//                block()
+//            }
+//        }catch (e:Exception){
+//            e.printStackTrace()
+//        }
+//    }
 }
